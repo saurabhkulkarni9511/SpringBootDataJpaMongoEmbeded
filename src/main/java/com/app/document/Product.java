4588fdfb46da70734131f3105997bd67e344f34c
user <user@DESKTOP-5L8E5H1>
2019-06-22 12:00:01 +0530

    code send

---
+++ src/main/java/com/app/document/Product.java
@@ -0,0 +1,65 @@
+package com.app.document;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Product {
+
+	@Id
+	private String id;
+
+	private Integer prodId;
+	private String prodCode;
+	private double prodCost;
+
+	public Product() {
+		super();
+	}
+
+	public Product(Integer prodId, String prodCode, double prodCost) {
+		super();
+		this.prodId = prodId;
+		this.prodCode = prodCode;
+		this.prodCost = prodCost;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Integer getProdId() {
+		return prodId;
+	}
+
+	public void setProdId(Integer prodId) {
+		this.prodId = prodId;
+	}
+
+	public String getProdCode() {
+		return prodCode;
+	}
+
+	public void setProdCode(String prodCode) {
+		this.prodCode = prodCode;
+	}
+
+	public double getProdCost() {
+		return prodCost;
+	}
+
+	public void setProdCost(double prodCost) {
+		this.prodCost = prodCost;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", prodId=" + prodId + ", prodCode=" + prodCode + ", prodCost=" + prodCost + "]";
+	}
+
+
+}
---
+++ src/main/java/com/app/runner/ConsoleRunner.java
@@ -0,0 +1,28 @@
+package com.app.runner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.app.document.Product;
+import com.app.repo.ProductRepository;
+
+@Component
+public class ConsoleRunner implements CommandLineRunner{
+
+	@Autowired
+	private ProductRepository repo;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		repo.deleteAll();
+
+		repo.save(new Product(10,"A",2.2));
+		repo.save(new Product(11,"B",3.2));
+		repo.save(new Product(12,"C",4.2));
+
+		System.out.println("-----------");
+		repo.findAll().forEach(System.out::println);
+	}
+}
